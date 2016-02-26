92db35290bd5a7a50a2d4b320e32eb36c0882f5b
Tom MacWright <tom@macwright.org>
2016-02-26 15:11:42 -0500

    Better shoutout to @katydecorah :)

--- data/forbidden.js
+++ data/forbidden.js
@@ -1,3 +1,13 @@
+/**
+ * These words are taken from
+ *
+ * https://github.com/katydecorah/copy-cop/blob/gh-pages/_data/omit.yml
+ *
+ * and those words are taken from
+ *
+ * http://www.plainlanguage.gov/howto/wordsuggestions/simplewords.cfm
+ * https://css-tricks.com/words-avoid-educational-writing/
+ */
 module.exports = [
     'easy',
     'easily',
